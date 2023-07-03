57aec9e2d939e7a642280f8f4ae1e6b66fe44141
Jmi1990 <jmijan1990@gmail.com>
2023-07-03 09:46:18 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,48 +1,35 @@
 #include "main.h"
 
 /**
-
- * _strstr - Entry point
-
- * @haystack: input
-
- * @needle: input
-
- * Return: Always 0 (Success)
-
+ * _strstr - finds the first occurrence of the substring needle in haystack
+ * @haystack: string where to look
+ * @needle: string to find
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 char *_strstr(char *haystack, char *needle)
-
 {
+	int l, l2, i, j, c;
 
-        for (; *haystack != '\0'; haystack++)
-
-        {
-
-                char *l = haystack;
-
-                char *p = needle;
-
-
-                while (*l == *p && *p != '\0')
-
-                {
-
-                        l++;
-
-                        p++;
-
-                }
-
-
-                if (*p == '\0')
-
-                        return (haystack);
-
-        }
-
-
-        return (0);
-
+	l = 0, l2 = 0, c = 0;
+	while (haystack[l] != '\0')
+		l++;
+	while (needle[l2] != '\0')
+		l2++;
+	for (i = 0; i < l; i++, haystack++)
+	{
+		for (j = 0, c = 0; j < l2; j++)
+		{
+			if (haystack[j] == 0)
+				break;
+			if (haystack[j] == needle[j])
+			{
+				c += 1;
+			}
+		}
+		if (c == l2)
+			return (haystack);
+	}
+	return (0);
 }
